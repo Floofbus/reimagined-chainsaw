2013e1046c5ebd5113e6aefeb0abfa2a9167623c
Kandit <floofbus@gmail.com>
2018-11-29 11:44:24 -0600

    dailyprogrammer 10e

---
+++ 10e/main.cpp
@@ -0,0 +1,40 @@
+// https://www.reddit.com/r/dailyprogrammer/comments/pv98f/2182012_challenge_10_easy/
+// /r/dailyprogrammer challenge #10, easy.
+#include <iostream>
+#include <string>
+#include <regex>
+
+using namespace std;
+
+bool validateNumber(const string &s)
+{
+    regex tenDigit("[(]{0,1}\\d\\d\\d[)-\\.]{0,1}[\\s]{0,1}\\d\\d\\d[-\\s\\.]{0,1}\\d\\d\\d\\d");
+    regex sevenDigit("\\d\\d\\d[-\\s\\.]{0,1}\\d\\d\\d\\d");
+    if (regex_match(s, tenDigit) || regex_match(s, sevenDigit))
+        return true;
+    
+    return false;
+}
+
+int main()
+{
+    cout << "Enter a phone number: ";
+    string number;
+    getline(cin, number);
+    
+    if (validateNumber(number))
+        cout << "It was a valid number.";
+    else
+        cout << "It was not a valid number.";
+        
+    return 0;
+}
+
+// Notes:
+// Pretty simple one today. I had to brush up on my regex (but then again, who doesn't?)
+// My approach was to define two different regex strings to check against, one for 10 digit
+// numbers, and one for 7 digit numbers.
+// 
+// Things that could be improved:
+// The matching is very loose. I could make validateNumber construct a single regex string
+// to match. Right now you could input stuff like (123.456 789 and it would match just fine.
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 .PHONY: clean All
 
 All:
-	@echo "----------Building project:[ 9e - Debug ]----------"
-	@cd "9e" && "$(MAKE)" -f  "9e.mk"
+	@echo "----------Building project:[ 10e - Debug ]----------"
+	@cd "10e" && "$(MAKE)" -f  "10e.mk"
 clean:
-	@echo "----------Cleaning project:[ 9e - Debug ]----------"
-	@cd "9e" && "$(MAKE)" -f  "9e.mk" clean
+	@echo "----------Cleaning project:[ 10e - Debug ]----------"
+	@cd "10e" && "$(MAKE)" -f  "10e.mk" clean
